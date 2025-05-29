3c49692f08d7c1b7011c87d7b537f62c43e2f62d
lolejniczak <lolejniczak@google.com>
2025-05-29 16:35:04 +0000

    upgraded to 1.1.0

--- adk04-db-toolbox/agent.py
+++ adk04-db-toolbox/agent.py
@@ -2,9 +2,7 @@ import os
 from dotenv import load_dotenv
 from google.adk.agents import Agent
 from google.genai import types
-from google.adk.agents import Agent
-from google.adk.tools.toolbox_tool import ToolboxTool
-
+from google.adk.tools.toolbox_toolset import ToolboxToolset
 
 load_dotenv()
 
@@ -12,21 +10,18 @@ instruction_prompt = """
         You are a helpful agent who can answer user questions about the hotels in a specific city or hotels by name. Use the tools to answer the question"
         """
 
-
 MODEL = "gemini-2.0-flash-001"
 AGENT_APP_NAME = 'travelexpert'
 
-toolbox = ToolboxTool("https://dbtoolbox-680248386202.us-central1.run.app")
-
+toolbox = ToolboxToolset("https://dbtoolbox-680248386202.us-central1.run.app", toolset_name = "my_first_toolset")
 # Load single tool
 # tools = toolbox.get_tool(tool_name='search-hotels-by-location'),
 # Load all the tools
-dbtools = toolbox.get_toolset(toolset_name='my_first_toolset')
 
 root_agent = Agent(
         model=MODEL,
         name=AGENT_APP_NAME,
         description="Agent to answer questions about hotels in a city or hotels by name.",
         instruction=instruction_prompt,
-        tools=dbtools
+        tools=[toolbox]
 )
